d40fe07fc3be98337828095fca38fd84a4b395e4
Person-0 <atulyajain28122007@gmail.com>
2026-04-25 23:42:33 +0530

    cleanup stuff

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 const canvas = document.getElementById('main') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
+const canvasWidth = 512;
+const canvasHeight = 512;
+const vCenter = canvasHeight / 2;
+const hCenter = canvasWidth / 2;
+
 const resize = () => {
     canvas.width = 512;
     canvas.height = 512;
@@ -28,33 +33,34 @@ const noise = (x: number) => {
 const terrain = (x: number) => noise(x * 0.0067) * 128;
 
 function draw() {
-    ctx.clearRect(0, 0, 512, 512);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    for (let x = 0; x < 512; x += 1) {
+    for (let x = 0; x < canvasWidth; x += 1) {
         const point = x + offset;
 
         ctx.fillRect(
             x,
-            512 / 2 + terrain(point),
+            vCenter + terrain(point),
             2,
             2
         );
     }
 
-    const mid = 512/2 + offset;
-    const midL = terrain(mid - 0.25);
-    const midR = terrain(mid + 0.25);
-    ctx.fillRect(512/2-2, 512/2 + terrain(mid), 4, 4);
-    
-    const y = 512 / 2 + terrain(mid);
+    const midP = hCenter + offset;
+    const mid = terrain(midP);
+    const midL = terrain(midP - 0.25);
+    const midR = terrain(midP + 0.25);
+    ctx.fillRect(hCenter - 2, vCenter + mid, 4, 4);
+
+    const y = vCenter + mid;
     const slope = (midR - midL) / 0.5;
     const dx = 20;
     const dy = slope * dx;
     ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.lineWidth = 5;
-    ctx.moveTo(512 / 2 - dx, y - dy);
-    ctx.lineTo(512 / 2 + dx, y + dy);
+    ctx.moveTo(hCenter - dx, y - dy);
+    ctx.lineTo(hCenter + dx, y + dy);
     ctx.stroke();
 
     offset += speed;
